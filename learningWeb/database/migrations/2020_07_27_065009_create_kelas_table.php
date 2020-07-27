a9813de22a471f19b1d9bf36444505e337564330
IritaSee <iga.narendra@gmail.com>
2020-07-27 15:34:33 +0800

    making controllers but still cant change auth view

--- learningWeb/database/migrations/2020_07_27_065009_create_kelas_table.php
+++ learningWeb/database/migrations/2020_07_27_065009_create_kelas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateKelasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('kelas', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('sekolah');
-            $table->string('email')->unique();
-            $table->integer('status');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('nama');
             $table->timestamps();
         });
     }
@@ -33,6 +27,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('kelas');
     }
 }
